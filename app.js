37b87e05764a24dabb2ea728e9578e56e5641aab
Ethan Hansen <hanseneg@hotmail.com>
2020-09-29 15:52:05 -0600

    commit2

---
+++ app.js
@@ -0,0 +1,26 @@
+//es6 practice
+//beginner------------------------------------------------
+
+//rest operator
+const collectAnimals = (...animals) => animals
+// console.log(collectAnimals('dog', 'cat', 'bird', 'bear'))
+
+
+//Object Literals
+const foods = (grains, fruits, meats, vegetables, dairy) => {
+    return{
+        grains, fruits, meats, vegetables, dairy
+    }
+}
+console.log(foods(['oats', 'flour', 'flax'], ['apple', 'pear', 'banana', 'orange'], ['pork', 'steak', 'chicken'], ['potato', 'corn'], ['milk', 'ice cream']))
+
+//destructuring
+
+
+//intermediate--------------------------------------------
+
+
+//expert--------------------------------------------------
+
+
+//expert only---------------------------------------------
