bf7e0b63a9c60f5f55d1c8f2612365e5fdd0b431
Ethan Hansen <hanseneg@hotmail.com>
2020-09-29 17:21:40 -0600

    commit4

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //es6 practice
-//beginner------------------------------------------------
+//beginner-------------------------------------------------
 
 //rest operator
 const collectAnimals = (...animals) => animals
@@ -27,17 +27,76 @@ const returnFirst = (...items) => {
 
 const favoriteActivities = ["magnets", "snowboarding", "philanthropy", "janitor work", "eating"];
 
-const returnFavorites = (arr) => {
+const returnFavorites = () => {
     const [firstFav, secondFav, thirdFav] = favoriteActivities
     return `My top three favorite activities are, ${firstFav}, ${secondFav}, and ${thirdFav}.`
 }
-console.log(returnFavorites(favoriteActivities))
+// console.log(returnFavorites(favoriteActivities))
 
 
 //intermediate--------------------------------------------
 
+const realAnimals = ["dog", "cat", "mouse"];
+const magicalAnimals = ["jackolope"];
+const mysteriousAnimals = ["platypus"];
+
+const combineAnimals = () => {  
+    return [...realAnimals, ...magicalAnimals, ...mysteriousAnimals]
+}
+// console.log(combineAnimals(realAnimals, magicalAnimals, mysteriousAnimals)); 
+
+// ["dog", "cat", "mouse", "jackolope", "platypus"]
+
 
 //expert--------------------------------------------------
 
+// origninal
+// function product(a, b, c, d, e) {  
+//     var numbers = [a,b,c,d,e];
+  
+//     return numbers.reduce(function(acc, number) {
+//       return acc * number;
+//     }, 1)
+//   }
+
+// new
+const product = (...numbers) => numbers.reduce((acc, number) => acc * number, 1)
+// console.log(product(1,2,3,4,5,6))
+
+// original
+// function unshift(array, a, b, c, d, e) {  
+//     return [a, b, c, d, e].concat(array);
+//   }
+
+//  new
+const unshift = (arr, ...array2) => {  
+    return [...arr, ...array2]
+  }
+// console.log(unshift([5,6,7,8], 4))
 
 //expert only---------------------------------------------
+
+// original
+// function populatePeople(names){
+//     return names.map(function(name){
+//         name = name.split(" ");
+//         // your code
+//         return {
+//             firstName: firstName,
+//             lastName: lastName
+//         }
+//     })
+// }
+//
+//populatePeople(["Frank Peterson", "Suzy Degual", "Liza Jones"])
+
+// new
+
+const populatePeople = (names) => names.map(name => {
+    const [firstName, lastName] = name.split(" ")
+        return {
+            firstName,
+            lastName
+        }
+    })
+// console.log(populatePeople(["Frank Peterson", "Suzy Degual", "Liza Jones"]))
